6053a1aad7d0ed21c27b8f3b4755bb106a170f7c
zzz <17704904@qq.com>
2021-12-14 15:13:16 +0800

    first

---
+++ src/helpers/bus.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+
+export default new Vue()
+
+
+/*
+使用方法
+
+import Bus from '../helpers/bus'
+
+Buts.$on('test',msg=> {
+    console.log(msg)
+})
+
+Bus.$emit('test','hello')
+
+*/
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/Login'
+import Notebooks from '../components/NotebookList'
+import NoteDetail from '../components/NoteDetail'
+import TrashDetail from '../components/TrashDetail'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path:'/login',
+      component:Login
+    },
+    {
+      path:'/notebooks',
+      component:Notebooks
+    },
+    {
+      path:'/note/:noteId',
+      component:NoteDetail
+    },
+    {
+      path:'/trash/:noteId',
+      component:TrashDetail
+    }
+  ]
+})
